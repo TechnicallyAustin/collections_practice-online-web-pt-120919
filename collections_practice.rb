7a0a6051e7f3d3ac8e25b59cee850ec1908f7921
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 06:11:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,7 @@ def kesha_maker (array)
   array.each do |i|
     word_arr = i.split(" ")
     word_arr[2] << "$"
-    new_arr << word_arr.join
+    new_arr << "#{word_arr.join}"
 end
 return new_arr
 end
